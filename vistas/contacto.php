a6148b13a2cee288e8c8d835872c524d7e160998
champiiy <albert1047@hotmail.com>
2017-02-12 23:50:47 -0600

    corrigiendo input number

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -63,7 +63,7 @@
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
                             <div class="col-md-8">
-                                <input id="phone" name="phone" type="number" min="7" max="10" placeholder="Teléfono" class="form-control" required/>
+                                <input id="phone" name="phone" type="number" placeholder="Teléfono" class="form-control" required/>
                             </div>
                         </div>
 
