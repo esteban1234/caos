bfe466571ec42a41a4149edccb335eb441bfa8a9
esteban1234 <tva_libra@hotmail.com>
2017-03-06 17:26:20 -0600

    añadido de las verificaciones

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="google-site-verification" content="487htsFqRY73gJNThKvmYsecIZAWjl9_opfW_OTN9I0" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>CORPORATIVO, ADMINISTRATIVO Y OPERATIVO DEL SUR</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <META NAME="Title" content="CAOS en ciudad de mexico.">
-    <META NAME="Author" CONTENT="[]" />
+    <META NAME="Author" CONTENT="[http://corporativocaos.com/]" />
     <META name="Description" content="Somos una compañía ubicada en la ciudad de méxico, méxico, dedicados a ofrecer servicios a personas físicas o morales toda clase de consultorías y asesoría, así como realizar estudios en materia económica, administrativa, financiera, técnica, industrial, contable y legal. Servicios profesionales en las áreas de derecho fiscal, mercantil, laboral, civil, penal, corporativo, migratorio, seguridad social, mediante defensas directas ante todo tipo de autoridades. Servicios en materia contable, fiscal, financiera, administrativa, capacitación de personal y la realización de actos jurídicos y contratos." />
     <META NAME="Keywords" CONTENT="estudios económicos en méxico, estudios financieros en méxico, legal, organización legal, legal en méxico, legal, fiscal." />
   	<link rel="stylesheet" href="../css/bootstrap.css">
